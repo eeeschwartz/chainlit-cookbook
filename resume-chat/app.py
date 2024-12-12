012d80154a12809f579da5f1d3fffa980b4f43f0
Erik Schwartz <erik@erikschwartz.net>
2024-12-12 17:28:42 -0500

    Stream results from gemini

--- resume-chat/app.py
+++ resume-chat/app.py
@@ -22,30 +22,38 @@ async def on_chat_start():
     )
     cl.user_session.set("agent", agent)
 
-    res = await cl.AskUserMessage(content="How can I help you?", timeout=30).send()
-    if res is None:
-        await cl.Message(content="No response received. Please try again.").send()
-        return
+    # res = await cl.AskUserMessage(content="How can I help you?", timeout=30).send()
+    # if res is None:
+    #     await cl.Message(content="No response received. Please try again.").send()
+    #     return
 
-    result = agent.run_sync(res["output"])
-    await cl.Message(content=result.data).send()
+    # result = agent.run_sync(res["output"])
+    # await cl.Message(content=result.data).send()
 
 @cl.on_message
 async def on_message(message: cl.Message):
     agent = cl.user_session.get("agent")  # type: Agent
-    print("Agent:")
-    print(agent)
 
-    # Get previous messages to maintain context
-    previous_messages = []
-    if hasattr(agent, "last_run_messages") and agent.last_run_messages:
-        previous_messages = agent.last_run_messages
-        print("Previous messages:")
-        print(previous_messages)
-    else:
-        print("No previous messages")
+    prompt = UserPrompt(content=message.content).content
+    
+    await cl.Message(content=UserPrompt(content=prompt)).send()
+    async with agent.run_stream(prompt, message_history=[]) as result:
+            async for text in result.stream(debounce_by=0.01):
+                # text here is a `str` and the frontend wants
+                # JSON encoded ModelTextResponse, so we create one
+                m = ModelTextResponse(content=text, timestamp=result.timestamp())
+                await cl.Message(content=m.content).send()
 
-    # Create response message placeholder
-    result = agent.run_sync(message.content)
-    res = cl.Message(content=result.data)
-    await res.send()
+    # # Get previous messages to maintain context
+    # previous_messages = []
+    # if hasattr(agent, "last_run_messages") and agent.last_run_messages:
+    #     previous_messages = agent.last_run_messages
+    #     print("Previous messages:")
+    #     print(previous_messages)
+    # else:
+    #     print("No previous messages")
+
+    # # Create response message placeholder
+    # result = agent.run_sync(message.content)
+    # res = cl.Message(content=result.data)
+    # await res.send()
